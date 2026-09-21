ad1b3d5be0322cafcb6872459908143b528cd147
cwlewis299 <cwlewis299@gmail.com>
2026-09-21 13:43:19 -0500

    Add OpenRouter chat completion request

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
 
-load_dotenv()
-
-api_key = os.environ.get("OPENROUTER_API_KEY")
-
-if api_key is None:
-    raise RuntimeError("OPENROUTER_API_KEY environment variable is not set. Please set it in your .env file.")
 
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("OPENROUTER_API_KEY environment variable is not set. Please set it in your .env file.")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key
+    )
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ],
+    )
+
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
