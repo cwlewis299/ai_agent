f52b1f843f14808402c7ee7a5d02fd8f9c4896dc
cwlewis299 <cwlewis299@gmail.com>
2026-09-23 21:40:24 -0500

    Add system prompt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from prompts import system_prompt
 from dotenv import load_dotenv
 from openai import OpenAI
 
@@ -21,10 +22,8 @@ def main():
     args = parser.parse_args()
 
     messages = [
-        {
-            "role": "user",
-            "content": args.user_prompt,
-        }
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": args.user_prompt},
     ]
 
     response = client.chat.completions.create(
